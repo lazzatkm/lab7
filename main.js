f7ca1a90729c1bc6b09e876c12b3288c91c36897
Lazzat Kenges <lazzatkenges@Lazzats-MacBook-Air.local>
2023-03-13 17:19:04 +0600

    message

---
+++ main.js
@@ -0,0 +1,29 @@
+window.onload = function() {
+    var paragraph = document.getElementById("lazzat1");
+    paragraph.innerHTML = "";
+  }
+  
+document.write("<h1 style='color:blue;'>Lab7 Assignment </h1>");
+
+document.write("<hr/>");
+
+document.write("<h2 style='color:red;'>Task </h2>");
+document.write("<p>In this task you have to reproduce this HTML page as is using only JavaScript. The task will be evaluated on the basis of the following DOM manipulation techniques:</p>");
+document.write("<li style='color:green;'>find elements in the DOM <strong>(5 points)</strong>;</li>");
+document.write("<li style='color:purple;'>create/add/remove elements <strong>((5 points)</strong>;</li>");
+document.write("<li style='color:green;'>change content of the elements <strong>((5 points)</strong>;</li>");
+document.write("<li style='color:purple;'>change styles of the elements <strong>((5 points)</strong>;</li>");
+document.write("<li style='color:green;'>change attributes of the elements <strong>((5 points)</strong>;</li>");
+document.write("<li style='color:purple;'>change classes of the elements <strong>((5 points)</strong>.</li>");
+
+document.write("<hr/>");
+
+document.write("<h2 style='color:red;'> Submission </h2>");
+document.write("<p>To submit your work, follow these instructions:</p>");
+document.write("<li style='color:green;'>Create a new repository on Github, named <strong> lab7 (1 point)</strong>.</li>");
+document.write("<li style='color:purple;'>Clone this repository to your local machine and work inside it.</li>");
+document.write("<li style='color:green;'>Create a new HTML file, called <strong> index.html, </strong>which has only one  &lth1&gt tag with 'Hello, World!' message <strong>((1 point)</strong>.</li>");
+document.write("<li style='color:purple;'>Create a new JavaScript file, called <strong>main.js,</strong> which must contain your program (assignment) described above <strong>((1 point)</strong>.</li>");
+document.write("<li style='color:green;'>Link this  <strong>main.js </strong> file to your  <strong>index.html </strong> file Note: place your script at the end of the <strong> body </strong> section <strong>(1 point)</strong>.</li>");
+document.write("<li style='color:purple;'>Write a JavaScript program in <strong> main.js </strong> to make your<strong> index.html </strong> look identical to this HTML file <strong>((5 points)</strong>.</li>");
+document.write("<li style='color:green;'>After you finish your work, submit it to the Github  <strong>( 2 points)</strong>.</li>");
